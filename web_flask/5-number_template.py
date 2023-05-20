68252a74b01b942896a5846a44eb0f0cfa8e424d
Chidi <chidiekejiuba@gmail.com>
2023-05-20 06:22:49 +0100

    fixed spacing error

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,10 +35,12 @@ def number(n):
     """ displays n is a number if n is an int """
     return "%d is a number" % n
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ displays html page """
     return render_template("5-number.html", n=n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
