673f11682a4a9b3fe7deaf6a89beb7243e2ee312
Abdulbasit <aabdulrafiu721@gmail.com>
2023-08-05 06:37:38 +0100

    adding another route to the web app

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -15,5 +15,11 @@ def index():
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ returns Hbnb """
+    return 'HBNB'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
