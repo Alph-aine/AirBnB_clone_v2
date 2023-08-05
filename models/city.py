615df4e4ebb30dae8d54611bcbbc5eaab8a739a5
Abdulbasit <aabdulrafiu721@gmail.com>
2023-08-05 09:29:39 +0100

    testing

--- models/city.py
+++ models/city.py
@@ -1,30 +1,24 @@
-#!/usr/bin/python3
-""" City Module for HBNB project """
+#!/usr/bin/python
+""" holds class City"""
 import models
-import sqlalchemy
 from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, ForeignKey
-from models.place import Place
 from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class
-    Attributes:
-        state_id: id of the state
-        name: input nanme
-    """
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        __tablename__ = "cities"
+    """Representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship('Place', backref='cities')
+        places = relationship("Place", backref="cities")
     else:
-        state_id = ''
-        name = ''
+        state_id = ""
+        name = ""
 
     def __init__(self, *args, **kwargs):
-        '''initializes city'''
+        """initializes city"""
         super().__init__(*args, **kwargs)
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,7 @@
--- MySQL test server for this project
-CREATE DATABASE IF NOT EXISTS hbnd_test_db;
-CREATE USER IF NOT EXISTS 'hbn_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnd_test_db . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
+-- prepares a MySQL server for the project
 
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
